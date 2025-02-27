9360d6b20745496be761945370d1ffc0bce0e079
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-02-27 14:45:42 -0800

    added listen method

--- app.js
+++ app.js
@@ -18,22 +18,26 @@ app.set('view engine', 'ejs');
 const PORT = 3000;
 
 //Define a "default" route for our home page 
-app.get('/', (req,res) => 
-    {
-        //Send our home page as a response to the client
-        res.render(`home`); //render works with ejs files res.send is for html
-    });
+app.get('/', (req, res) => {
+    //Send our home page as a response to the client
+    res.render(`home`); //render works with ejs files res.send is for html
+});
 
 //create post
-app.post('/submit', (req, res) =>{
+app.post('/submit', (req, res) => {
 
-    const newPost ={
-    name: req.body.name,
-    title: req.body.title,
-    content: req.body.content
+    const newPost = {
+        name: req.body.name,
+        title: req.body.title,
+        content: req.body.content
     };
-    
+
     console.log(newPost)
 
     res.render('confirmation', { newPost });
+});
+
+//Send port in Console.
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
